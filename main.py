45dd85c6bbc095717410ef528c9a55b2585f8a3d
wkzawadzka <vee.zawadzka@gmail.com>
2024-12-02 10:10:55 +0100

    (main) add data pipeline

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
-from src.data_loader import load_data
-from src.pipeline import create_pipeline
-from src.Preprocessing import Preprocessing
-from src.CleoraFacade import CleoraFacade
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report
 from pathlib import Path
+from src.utils.data import download_data, make_preprocessed_edges_file, split_data, load_data
+from src.utils.plots import visualize_pipeline
+from src.pipelines.data import DataPipeline
+from src.pipelines.model import model_pipeline
+from src.cleora import CleoraFacade
 
 def main():
-    preprocessing_singleton = Preprocessing()
-    preprocessing_singleton.make_preprocessed_edges_file()
+    # prepare data
+    data_pipeline = DataPipeline([
+        ("Download data", download_data),
+        ("Preprocess edges", make_preprocessed_edges_file),
+        ("Split data", split_data),
+        ("Load Data", load_data)
+    ])
+    X_train, X_test, y_train, y_test = data_pipeline.run()
+    visualize_pipeline(data_pipeline, type='html2')
+
+
     cleora = CleoraFacade()
     cleora.run_cleora(Path.cwd() / "data" / "preprocessed_edges.txt")
     
-    X_train, X_test, y_train, y_test = load_data()
-    
+    # prepare models
     models = {
         "KNeighbors": KNeighborsClassifier(n_neighbors=7), 
         "DecisionTree": DecisionTreeClassifier() 
     }
     
     for model_name, model in models.items():
-            print(f"\nTraining pipeline with {model_name}...")
-            pipeline = create_pipeline(model)
-            
-            pipeline.fit(X_train, y_train)
-            y_pred = pipeline.predict(X_test)
-            
-            # evaluate model performance
-            print(f"\n{model_name} Model Evaluation:")
-            print(f"Accuracy: {pipeline.score(X_test, y_test):.4f}")
-            print(classification_report(y_test, y_pred))
+        print(f"\nTraining pipeline with {model_name}...")
+        pipeline = model_pipeline(model)
+        
+        pipeline.fit(X_train, y_train)
+        y_pred = pipeline.predict(X_test)
+        
+        # evaluate model performance
+        print(f"\n{model_name} Model Evaluation:")
+        print(f"Accuracy: {pipeline.score(X_test, y_test):.4f}")
+        print(classification_report(y_test, y_pred))
         
 if __name__ == '__main__':
     main()
